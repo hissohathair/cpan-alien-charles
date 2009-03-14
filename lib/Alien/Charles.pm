fe3099729724d92706b8b7ca98579c955954dc33
Daniel Austin <daniel.austin@gmail.com>
2009-03-14 23:21:34 +1100

    More tests and a bit saner interface

--- lib/Alien/Charles.pm
+++ lib/Alien/Charles.pm
@@ -1,10 +1,15 @@
+# Alien::Charles
+# Interact with the Charles Web Debugging Proxy
+
 package Alien::Charles;
 
 use warnings;
 use strict;
 
 use Carp;
+use Readonly;
 use File::Spec;
+use Params::Validate;
 use Mozilla::ProfilesIni;
 
 =head1 NAME
@@ -18,7 +23,6 @@ Version 0.01
 =cut
 
 our $VERSION = '0.01';
-
 
 =head1 SYNOPSIS
 
@@ -40,7 +44,33 @@ instance:
     use LWP::UserAgent;
     my $ua = LWP::UserAgent->new();
     my $chls = Alien::Charles->new();
-    $ua->proxy( 'http', $chls->proxy_url() );
+    if ( $chls->is_charles_running ) {
+        $chls->init_from_charles_port;
+        $ua->proxy( 'http', $chls->proxy_url );
+    }
+
+=head1 DESCRIPTION
+
+From L<http://www.charlesproxy.com/>
+
+    Charles is an HTTP proxy / HTTP monitor / Reverse Proxy that
+    enables a developer to view all of the HTTP traffic between their
+    machine and the Internet. This includes requests, responses and
+    the HTTP headers (which contain the cookies and caching
+    information).
+
+Charles is a shareware application developed by Karl von Randow at
+XK72 Ltd.
+
+Charles also has a Firefox plug-in available. This Firefox plug-in
+allows Charles to automatically take over Firefox's proxy settings
+whilst it is running, and restores them when Charles exits (or the
+user decides to stop proxying through Charles).
+
+This Perl module uses this feature to allow Perl programs to also
+automatically proxy through Charles by mimicing the behaviour of the
+Firefox plug-in.
+
 
 =head1 CONSTRUCTOR
 
@@ -52,35 +82,63 @@ TODO: Why? Why not just a procedural interface?
 
 =cut
 
-our $DEFAULT_PORT = 8888;
+our $DEFAULT_PORT = '8888';
+our $DEFAULT_HOST = 'localhost';
 
-sub new
-{
-    my( $class ) = @_;
-    my $self = { };
+sub new {
+    my @args  = @_;
+    my $class = shift @args;
+    my $self  = {};
     bless $self, $class;
-    $self->{port} = 8888;
-    $self->{firefox_profile} = $self->_get_firefox_profile_path();
+
+    # Take a hash of init values
+    %{$self} = validate(
+        @args,
+        {
+            port            => { default => $DEFAULT_PORT },
+            host            => { default => $DEFAULT_HOST },
+            firefox_profile => { default => q{-} },
+        }
+    );
+
+    # Value of '-' means "you tell me" -- so process it twice
+    $self->firefox_profile_path( $self->firefox_profile_path() );
+
     return $self;
 }
 
-=head1 METHODS
+=head1 SUBROUTINES/METHODS
 
 =head2 port
 
 Get/set the TCP port number that Charles is listening on.
 
+=head2 host
+
+Get/set the hostname.
+
 =cut
 
-sub port
-{
-    my( $self, $port ) = @_;
-    if ( defined $port ) {
-	$self->{port} = $port;
+# These two routines are very similar and I used to use AUTOLOAD for this but...
+# ...it causes traps and problems for the unwary.
+
+sub _set_field {
+    my ( $self, $key, $val ) = @_;
+    if ( defined $val ) {
+        $self->{$key} = $val;
     }
-    return $self->{port};
+    return $self->{$key};
 }
 
+sub port {
+    my $self = shift @_;
+    return $self->_set_field( 'port', @_ );
+}
+
+sub host {
+    my $self = shift @_;
+    return $self->_set_field( 'host', @_ );
+}
 
 =head2 proxy_url
 
@@ -88,12 +146,57 @@ Return URL string that represents Charles' proxy URL.
 
 =cut
 
-sub proxy_url
-{
-    my( $self ) = @_;
-    return 'http://localhost:' . $self->port() . '/';
+sub proxy_url {
+    my ($self) = @_;
+    return 'http://' . $self->host() . q{:} . $self->port() . q{/};
 }
 
+=head2 is_charles_running
+
+Returns true (1) if we B<think> Charles is running, based on the
+presence of a file called C<charles_port> in the currently active
+Firefox profile.
+
+=cut
+
+sub is_charles_running {
+    my ($self) = @_;
+    return ( -f $self->charles_port() ? 1 : 0 );
+}
+
+=head2 init_from_charles_port 
+
+If Charles is running, then we can find out what port it's listening
+on by checking the C<charles_port> file in the Firefox profile
+directory. If such a file exists, we parse it and initialise our
+objects values.
+
+=cut
+
+sub init_from_charles_port {
+    my ($self) = @_;
+
+    my $init_ok = 0;
+    if ( $self->is_charles_running ) {
+        if ( open my $cf, '<', $self->charles_port ) {
+            my $line = <$cf>;
+            close $cf
+              or carp "close: Error closing charles_port ($!)";
+
+            if ( $line =~ m{(\d+) \s+ -1}xsm ) {
+                $self->port($1);
+                $init_ok = 1;
+            }
+            else {
+                carp $self->charles_port . ': unable to parse file';
+            }
+        }
+        else {
+            carp 'open: Cannot read ' . $self->charles_port . " ($!)";
+        }
+    }
+    return $init_ok;
+}
 
 =head2 start
 
@@ -105,25 +208,25 @@ listening on).
 
 =cut
 
-sub start
-{
-    my( $self ) = @_;
+Readonly my $CHARLES_MYSTERY_MEAT => -1;
+
+sub start {
+    my ($self) = @_;
     if ( -d $self->{firefox_profile} ) {
-	if ( open my $cf, '>', $self->charles_port() ) {
-	    print $cf join(' ', $self->port(), -1);
-	    print $cf "\n\n";
-	    close $cf;
-	} 
-	else {
-	    croak "open: Unable to open " . $self->charles_port() . " ($!)";
-	}
+        if ( open my $cf, '>', $self->charles_port() ) {
+            print {$cf} join q{ }, $self->port(), $CHARLES_MYSTERY_MEAT;
+            print {$cf} "\n\n";
+            close $cf or carp "close: Error closing charles port ($!)";
+        }
+        else {
+            croak 'open: Unable to open ' . $self->charles_port() . " ($!)";
+        }
     }
     else {
-	croak "start(): No such directory " . $self->{firefox_profile};
+        croak 'start(): No such directory ' . $self->{firefox_profile};
     }
     return 1;
 }
-
 
 =head2 stop
 
@@ -131,16 +234,17 @@ End the session (restore's Firefox's proxy settings to original).
 
 =cut
 
-sub stop
-{
-    my( $self ) = @_;
+sub stop {
+    my ($self) = @_;
     if ( -f $self->charles_port() ) {
-	if ( ! unlink $self->charles_port() ) {
-	    carp "unlink: Unable to remove " . $self->charles_port() . " ($!)";
-	}
-    } 
+        if ( !unlink $self->charles_port() ) {
+            carp 'unlink: Unable to remove ' . $self->charles_port() . " ($!)";
+        }
+    }
     else {
-	carp "stop(): Could not find " . $self->charles_port() . ". Did you start()?";
+        carp 'stop(): Could not find '
+          . $self->charles_port()
+          . '. Did you start()?';
     }
     return 1;
 }
@@ -151,39 +255,38 @@ Get/set the path to the Firefox profile currently in use.
 
 =cut
 
-sub firefox_profile_path
-{
-    my( $self, $path ) = @_;
+sub firefox_profile_path {
+    my ( $self, $path ) = @_;
 
-    if ( defined $path && $path eq '-' ) {
-	$self->{firefox_profile} = $self->_get_firefox_profile_path();
+    if ( defined $path && $path eq q{-} ) {
+        $self->{firefox_profile} = $self->_get_firefox_profile_path();
     }
     elsif ( defined $path ) {
-	$self->{firefox_profile} = $path;
+        $self->{firefox_profile} = $path;
     }
     return $self->{firefox_profile};
 }
 
-sub _get_firefox_profile_path
-{
-    my( $self ) = @_;
+sub _get_firefox_profile_path {
+    my ($self) = @_;
 
-
-    # '/Users/dan/Library/Application Support/Firefox/Profiles/v0wnq6rx.MozillaDebug';
-    #
+   # This is suggested in the Synopsis for Mozilla::ProfilesIni but Perl::Critic
+   # is not impressed.
+   #
     my $path = Mozilla::ProfilesIni::_find_profile_path(
-	home => $ENV{HOME},
-	type => 'firefox',
-	); 
+        home => $ENV{HOME},
+        type => 'firefox',
+    );
     my $ini = Mozilla::ProfilesIni->new( path => $path );
 
     my $cfg = Config::IniFiles->new( -file => $ini->ini_file() );
-    my $last_profile = $cfg->val('General', 'StartWithLastProfile', 0);
-    my $profile_name = $cfg->val("Profile$last_profile", 'Name', 'default');
-    
-    my $firefox_profile = $ini->profile_path( $profile_name );
+    my $last_profile = $cfg->val( 'General', 'StartWithLastProfile', 0 );
+    my $profile_name = $cfg->val( "Profile$last_profile", 'Name', 'default' );
+
+    my $firefox_profile = $ini->profile_path($profile_name);
+
     #warn $ini->ini_file();
-    
+
     return $firefox_profile;
 }
 
@@ -193,14 +296,10 @@ Return full path to the Charles "port" file.
 
 =cut
 
-sub charles_port
-{
-    my( $self ) = @_;
-    return File::Spec->catfile($self->{firefox_profile}, 'charles_port');
+sub charles_port {
+    my ($self) = @_;
+    return File::Spec->catfile( $self->{firefox_profile}, 'charles_port' );
 }
-
-
-=head1 DESTRUCTOR
 
 =head2 DESTROY
 
@@ -208,26 +307,56 @@ If the object goes out of scope it should clean-up after itself.
 
 =cut
 
-sub DESTROY
-{
-    my( $self ) = @_;
+sub DESTROY {
+    my ($self) = @_;
     if ( -f $self->charles_port() ) {
-	$self->stop();
+        $self->stop();
     }
+    return;
 }
+
+=head1 DIAGNOSTICS
+
+None given.
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+TBA
+
+=head1 DEPENDENCIES
+
+File::Spec
+
+Params::Validate
+
+Mozilla::ProfilesIni
+
+=head1 INCOMPATIBILITIES
+
+None noted.
+
+=head1 BUGS AND LIMITATIONS
+
+None noted. Yet.
+
+I suspect the method of determining which Firefox profile is "active"
+is idiosyncratic and not robust.
+
+Charles can also take over MSIE's proxy settings but this is not
+emulated in this module.
+
+It would be useful to support Safari, Chrome, Konquerer and other
+browsers as well I imagine.
+
+Please report any bugs or feature requests to C<bug-alien-charles at
+rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Alien-Charles>.  I
+will be notified, and then you'll automatically be notified of
+progress on your bug as I make changes.
 
 =head1 AUTHOR
 
 Daniel Austin, C<< <hisso at cpan.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-alien-charles at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Alien-Charles>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
 
 =head1 SUPPORT
 
@@ -261,8 +390,10 @@ L<http://search.cpan.org/dist/Alien-Charles/>
 
 =head1 ACKNOWLEDGEMENTS
 
+Obviously, props to Karl von Randow at XK72 Ltd for a great
+developer's tool.
 
-=head1 COPYRIGHT & LICENSE
+=head1 LICENSE AND COPYRIGHT
 
 Copyright 2009 Daniel Austin, all rights reserved.
 
@@ -272,4 +403,4 @@ under the same terms as Perl itself.
 
 =cut
 
-1; # End of Alien::Charles
+1;    # End of Alien::Charles
--- t/charles.t
+++ t/charles.t
@@ -1,68 +1,94 @@
 #!perl -w
-# $Id$
+# charles.t - Test Alien::Charles
 #
 
-use Test::More tests => 20;
+use Test::More tests   => 31;
 use constant TEST_PORT => 12345;
 
 BEGIN {
-    use_ok( 'Alien::Charles' );
+    use_ok('Alien::Charles');
 }
 
 # Constructor tests
 my $chls = Alien::Charles->new();
 isa_ok( $chls, 'Alien::Charles' );
 
-
 # Method tests
 
 # port()
-ok( $chls->port() == $Alien::Charles::DEFAULT_PORT, 'Default port should be 8888' );
-ok( $chls->port( TEST_PORT ) == TEST_PORT,          'port() should return new value'  );
-ok( $chls->port() == TEST_PORT,                     'port() should return old value' );
+is( $chls->port(), $Alien::Charles::DEFAULT_PORT,
+    'Default port should be 8888' );
+is( $chls->port(TEST_PORT), TEST_PORT, 'port() should return new value' );
+is( $chls->port(),          TEST_PORT, 'port() should return old value' );
 
 # proxy_url
 my $url = $chls->proxy_url();
-ok( $url eq 'http://localhost:' . TEST_PORT . '/', 'proxy() should be valid URL' );
+is( $url, 'http://localhost:' . TEST_PORT . '/',
+    'proxy() should be valid URL' );
 SKIP: {
-    eval { require Test::URI };
-    skip 'Test::URI not installed', 3 if $@;
+    eval { require URI };
+    skip 'URI not installed', 4 if $@;
 
-    Test::URI::uri_scheme_ok( $url, 'http' );
-    Test::URI::uri_host_ok(   $url, 'localhost' );
-    Test::URI::uri_port_ok(   $url, TEST_PORT );
+    my $uri = URI->new($url);
+    is( $uri->scheme, 'http',          'scheme should be http' );
+    is( $uri->host,   'localhost',     'host should be localhost' );
+    is( $uri->port,   TEST_PORT,       'port should be TEST_PORT' );
+    is( $url,         $uri->canonical, 'url should be canonical' );
 }
 
 # firefox_profile_path()
 my $orig_path = $chls->firefox_profile_path();
-ok( -d $chls->firefox_profile_path(), 'firefox_profile_path() should be a directory' )
+ok( -d $chls->firefox_profile_path(),             'firefox_profile_path() should be a directory' )
     or diag("Firefox profile path: $orig_path");
-is( $chls->firefox_profile_path( '.' ), '.',      'firefox_profile_path() should get set' );
+is( $chls->firefox_profile_path('.'),   '.',      'firefox_profile_path() should get set' );
 ok( -d $chls->firefox_profile_path(),             'firefox_profile_path() should work on cwd' );
 is( $chls->firefox_profile_path('-'), $orig_path, 'firefox_profile_path() should re-init' );
 
+# is_charles_running()
+my $charles_running = $chls->is_charles_running();
+ok( ( 1 == $charles_running ) || ( 0 == $charles_running ),
+    'is_charles_running() is either 1 or 0' );
+pass(   'Just so you know, Charles is '
+      . ( $charles_running ? '' : 'not ' )
+      . 'running' );
+
 # start()
-ok( $chls->start(),           'start() should work' );
+ok( $chls->start(), 'start() should work' );
+ok( $chls->is_charles_running(),
+    'charles should appear to be running after start()' );
 ok( -f $chls->charles_port(), 'charles_port() should exist after start()' );
 
 SKIP: {
     eval { require Test::File::Contents };
     skip 'Test::File::Contents not installed', 1 if $@;
 
-    Test::File::Contents::file_contents_is( $chls->charles_port(), 
-					    join(' ', TEST_PORT, -1) . "\n\n",
-					    'file should be correct format', );
-
+    Test::File::Contents::file_contents_is(
+        $chls->charles_port(),
+        join( ' ', TEST_PORT, -1 ) . "\n\n",
+        'file should be correct format',
+    );
 }
 
+is( $chls->port(0), 0, 'can change port after start' );
+ok( $chls->init_from_charles_port,
+    'should be able to re-init from port after start()' );
+is( $chls->port, TEST_PORT, 're-init restores port' );
 
 # stop()
-ok( $chls->stop(),              'stop() should work' );
-ok( ! -f $chls->charles_port(), 'charles_port() should NOT exist after stop()' );
-
+ok( $chls->stop(), 'stop() should work' );
+ok( !$chls->is_charles_running(),
+    'Charles should appear to be NOT running after stop()' );
+ok( !-f $chls->charles_port(), 'charles_port() should NOT exist after stop()' );
+ok( ! $chls->init_from_charles_port, 'should not be able to re-init after stop()' );
 
 # destroy()
-ok( $chls->start() && -f $chls->charles_port(), 'start() should be able to start after a stop()' );
+ok( $chls->start(),           'start() should be able to start after a stop()' );
+ok( -f $chls->charles_port(), 'start() should re-create the charles_port file' );
+
 my $file = $chls->charles_port();
 undef $chls;
-ok( ! -f $file, 'charles_port() should NOT exist after object goes out of scope' );
+ok( !-f $file,
+    'charles_port() should NOT exist after object goes out of scope' );
+
+$chls = Alien::Charles->new();
+isa_ok( $chls, 'Alien::Charles' );
